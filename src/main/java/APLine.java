74fd1c1697c9cb0a63077b08cfd12564dc3adaa2
ascendingmine <avyang@s.sfusd.edu>
2025-04-24 01:02:13 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -2,7 +2,7 @@ public class APLine
 {
   /* your code here */
   private int a,b,c;
-  public APLine(int a1, b1, c1){
+  public APLine(int a1, int b1, int c1){
     a = a1;
     b = b1;
     c = c1;
